d104caeeb8539d0f0106e44f6bd3c4996de6aed7
Ahmed El-Mahdi <agelmahdi@gmail.com>
2021-10-20 16:19:08 +0200

    [Recycler View][DiffUtil] enable only update those items that were different

---
+++ app/src/main/java/com/agelmahdi/newsapp/Adapter/NewsAdapter.kt
@@ -0,0 +1,68 @@
+package com.agelmahdi.newsapp.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.agelmahdi.newsapp.Model.NewsModel.Article
+import com.agelmahdi.newsapp.R
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_article_preview.view.*
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    // anonymous class that calc the differences between two lists
+    // enable only update those items that were different
+    // DiffUtil is asynchronous so it will execute that on a background tread, so it don't block the main tread
+    // used instead of passing a list of item and every time call notifyDataSetChanged that is always update the whole items even the items didn't change, and that very inefficient.
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            // use unique properties like id, url, ..etc
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_article_preview, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(ivArticleImage)
+            tvSource.text = article.source.name
+            tvTitle.text = article.title
+            tvDescription.text = article.description
+            tvPublishedAt.text = article.publishedAt
+
+            setOnItemClickListener {
+                onItemClickListener?.let { it(article) }
+            }
+        }
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    private fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+}
